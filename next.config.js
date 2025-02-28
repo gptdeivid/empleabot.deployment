ff43f1c42d008a75f43b394084233e72f7ece37d
David Cuellar <cuellardave.dev@outlook.com>
2025-02-28 12:03:31 -0600

    2

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
     
     return config;
   },
+  // Optimize for Azure Static Web Apps
+  poweredByHeader: false,
+  generateEtags: true,
+  compress: true,
 }
 
 module.exports = nextConfig 
